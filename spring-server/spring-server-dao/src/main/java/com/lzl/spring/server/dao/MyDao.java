e44b899fd328dbc8413b0afe2f3a2d099c341371
liaozeliang <muckliao@fenqile.com>
2017-08-02 16:17:07 +0800

    +1

---
+++ spring-server/spring-server-dao/src/main/java/com/lzl/spring/server/dao/MyDao.java
@@ -0,0 +1,5 @@
+package com.lzl.spring.server.dao;
+
+public interface MyDao {
+
+}
---
+++ spring-server/spring-server-impl/src/main/java/com/lzl/spring/server/impl/SpringServerServiceImpl.java
@@ -0,0 +1,12 @@
+package com.lzl.spring.server.impl;
+
+import java.util.Map;
+
+import com.lzl.spring.server.service.ISpringServerService;
+
+public class SpringServerServiceImpl implements  ISpringServerService{
+
+	public Object doWork(Map<String, Object> params){
+		return null;
+	}
+}
